aa98d07480fd762dd476318bb8c9f474cfe15c0b
stefan.galescu <stefan.galescu@gmail.com>
2024-05-17 11:44:35 +0300

    feat(tests): update test file
    - remove redundant `setUp` method
    - remove `@test` docblock
    - use `test_` method prefix
    - use `svg` helper instead of `file_get_contents` from vendor files

--- tests/HeroiconTest.php
+++ tests/HeroiconTest.php
@@ -9,11 +9,6 @@ use Statamic\Statamic;
 
 class HeroiconTest extends TestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
     public function render(string $variant, string $icon, array $attrs = [])
     {
         return Statamic::tag('heroicon')->params(array_merge(['variant' => $variant, 'icon' => $icon], $attrs))->fetch();
@@ -21,11 +16,10 @@ class HeroiconTest extends TestCase
 
     public function getSvgAsset(string $variant, string $icon)
     {
-        return file_get_contents(__DIR__.'/../vendor/blade-ui-kit/blade-heroicons/resources/svg/'.$variant[0].'-'.$icon.'.svg');
+        return svg('heroicon-'.$variant[0].'-'.$icon)->toHtml();
     }
 
-    /** @test */
-    public function can_render_heroicon_using_outline_variant()
+    public function test_can_render_heroicon_using_outline_variant()
     {
         $variant = 'outline';
         $icon = 'bars-3';
@@ -36,8 +30,7 @@ class HeroiconTest extends TestCase
         assertEquals(trim($render), trim($svg));
     }
 
-    /** @test */
-    public function can_render_heroicon_using_solid_variant()
+    public function test_can_render_heroicon_using_solid_variant()
     {
         $variant = 'solid';
         $icon = 'bars-3';
@@ -48,8 +41,7 @@ class HeroiconTest extends TestCase
         assertEquals(trim($render), trim($svg));
     }
 
-    /** @test */
-    public function can_render_heroicon_using_mini_variant()
+    public function test_can_render_heroicon_using_mini_variant()
     {
         $variant = 'mini';
         $icon = 'bars-3';
@@ -60,8 +52,7 @@ class HeroiconTest extends TestCase
         assertEquals(trim($render), trim($svg));
     }
 
-    /** @test */
-    public function can_add_attributes_to_svg()
+    public function test_can_add_attributes_to_svg()
     {
         $render = $this->render('outline', 'bars-3', ['class' => 'w-6 h-6', 'title="Main menu"']);
 
@@ -69,16 +60,14 @@ class HeroiconTest extends TestCase
         assertStringContainsString('title="Main menu"', $render);
     }
 
-    /** @test */
-    public function can_add_dynamically_binded_attributes_to_svg()
+    public function test_can_add_dynamically_binded_attributes_to_svg()
     {
         $render = $this->render('outline', 'bars-3', ['x-bind:class' => "true ? 'w-6 h-6' : 'w-5 h-5'"]);
 
         assertStringContainsString('x-bind:class="true ? \'w-6 h-6\' : \'w-5 h-5\'"', $render);
     }
 
-    /** @test */
-    public function will_not_throw_when_icon_name_is_invalid()
+    public function test_will_not_throw_when_icon_name_is_invalid()
     {
         $render = $this->render('outline', 'invalid-icon-name');
 
