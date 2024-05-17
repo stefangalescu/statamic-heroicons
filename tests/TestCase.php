fd2e118a908570a0426f28cea2da421f35df86b4
Stefan Galescu <52995748+stefangalescu@users.noreply.github.com>
2024-05-17 12:35:09 +0300

    feat: add Statamic 5 support (#7)
    * feat(deps): upgrade statamic, laravel and related deps
    
    * chore: migrate `phpunit.xml` to new format
    
    * chore: add phpunit cache folder to gitignore
    
    * feat(tests): update base test case
    
    * feat(tests): update test file
    
    - remove redundant `setUp` method
    - remove `@test` docblock
    - use `test_` method prefix
    - use `svg` helper instead of `file_get_contents` from vendor files
    
    * feat(ci): update `tests` workflow
    
    - test against PHP 8.3
    - test against Statamic 5
    - test against Laravel 11
    - upgrade `actions/checkout` to v4
    
    * chore(deps): upgrade `laravel/pint`
    
    * chore: linting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,57 +4,26 @@ namespace StefanGalescu\Heroicons\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Statamic\Extend\Manifest;
-use Statamic\Providers\StatamicServiceProvider;
-use Statamic\Statamic;
+use Statamic\Testing\AddonTestCase;
 use StefanGalescu\Heroicons\ServiceProvider;
 
-abstract class TestCase extends OrchestraTestCase
+abstract class TestCase extends AddonTestCase
 {
+    protected string $addonServiceProvider = ServiceProvider::class;
+
     protected function getPackageProviders($app)
     {
-        return [
-            BladeHeroiconsServiceProvider::class,
-            BladeIconsServiceProvider::class,
-            StatamicServiceProvider::class,
-            ServiceProvider::class,
-        ];
-    }
+        $providers = parent::getPackageProviders($app);
 
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Statamic' => Statamic::class,
-        ];
-    }
+        array_unshift($providers, BladeHeroiconsServiceProvider::class, BladeIconsServiceProvider::class);
 
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app->make(Manifest::class)->manifest = [
-            'stefangalescu/statamic-heroicons' => [
-                'id' => 'stefangalescu/statamic-heroicons',
-                'namespace' => 'StefanGalescu\\Heroicons',
-            ],
-        ];
+        return $providers;
     }
 
     protected function resolveApplicationConfiguration($app)
     {
         parent::resolveApplicationConfiguration($app);
 
-        $configs = [
-            'antlers', 'assets', 'cp', 'forms', 'static_caching',
-            'sites', 'stache', 'system', 'users',
-        ];
-
-        foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
-        }
-
         $app['config']->set('statamic.antlers.version', 'runtime');
-        $app['config']->set('statamic.users.repository', 'file');
     }
 }
