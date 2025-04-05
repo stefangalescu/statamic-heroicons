300cb1667ab9f2dd9d91f3ca06e144b6296ed48f
Stefan Galescu <stefan.galescu@pentest-tools.com>
2025-04-05 15:46:46 +0300

    feat: remove exception solution

--- src/Exceptions/IncorrectEngineException.php
+++ src/Exceptions/IncorrectEngineException.php
@@ -3,21 +3,13 @@
 namespace StefanGalescu\Heroicons\Exceptions;
 
 use Exception;
-use Facade\IgnitionContracts\ProvidesSolution;
-use Facade\IgnitionContracts\Solution;
-use StefanGalescu\Heroicons\Exceptions\Solutions\IncorrectEngineSolution;
 
-class IncorrectEngineException extends Exception implements ProvidesSolution
+class IncorrectEngineException extends Exception
 {
     protected $message;
 
     public function __construct()
     {
         $this->message = 'The `stefangalescu/statamic-heroicons` addon requires the `runtime` Antlers engine.';
-    }
-
-    public function getSolution(): Solution
-    {
-        return new IncorrectEngineSolution();
     }
 }
